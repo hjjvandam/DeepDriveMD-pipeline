52e42cdd0e54bc31168d4b03e98051f2293fd630
Hubertus Dam <hvandam@bnl.gov>
2024-07-05 11:06:53 -0400

    Probabilistically bring MD back to sanity

--- deepdrivemd/sim/nwchem/main3_nwchem.py
+++ deepdrivemd/sim/nwchem/main3_nwchem.py
@@ -8,11 +8,38 @@ These tests make sure that
 4. We can store the results in a format suitable for DeePMD
 '''
 
-import os
 import ase_nwchem
 import glob
-import sys
+import os
 from pathlib import Path
+import random
+import shutil
+import sys
+
+def prob_replace_pdb(total,success,good_pdb,start_pdb):
+    """
+    Probabilistically replace the MD start PDB file
+
+    Normally the MD tasks start from the last structure that needed
+    more training data from the previous MD run. This facilitates
+    exploring more of the molecular phase space, but it can also
+    take the calculation into rediculous regions of phase space.
+    These regions are typically characterized with most if not all
+    DFT calculations failing. This function probabilistically 
+    replaces the starting PDB structure with a good structure
+    based on the fraction of DFT calculations that fail.
+
+    total - the total number of DFT calculations
+    success - the number of successful DFT calculations
+    good_pdb - a file with a known good structure
+    start_pdb - the file with the MD starting structure
+    """
+    fails = total - success
+    fraction_fails = (1.0*fails)/(1.0*total)
+    rnd = random.uniform(0.0,1.0)
+    if rnd <= fraction_fails:
+        # Copy good_pdb over start_pdb
+        path = shutil.copy(good_pdb,start_pdb)
 
 # the NWCHEM_TOP environment variable needs to be set to specify
 # where the NWChem executable lives.
@@ -43,11 +70,14 @@ print("Extract NWChem results")
 test_dat = []
 with open("inputs.txt", "r") as fp:
     lines = fp.readlines()
+num_inputs = len(lines)
 for line in lines:
     filename = line.strip()
     test_out = Path(filename).with_suffix(".nwo")
     if os.path.exists(test_out):
         test_dat.append(test_out)
+num_success = len(test_dat)
+prob_replace_pdb(num_inputs,num_success,test_pdb,Path("tmp.pdb"))
 ase_nwchem.nwchem_to_raw(test_dat)
 print("Convert raw files to NumPy files")
 ase_nwchem.raw_to_deepmd(deepmd_source_dir)
