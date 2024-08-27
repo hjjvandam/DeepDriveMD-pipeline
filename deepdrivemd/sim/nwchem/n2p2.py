03dd7e7b9059063ff8d51fab0a44b866385676bb
Hubertus Dam <hvandam@bnl.gov>
2024-08-27 16:06:59 -0400

    Fixed the molecule output format

--- deepdrivemd/sim/nwchem/n2p2.py
+++ deepdrivemd/sim/nwchem/n2p2.py
@@ -11,70 +11,85 @@ def create_file(filename):
     with open(filename, 'w') as file:
         file.write("")  # Create an empty file
 
-def write_to_file(filename, molecule_name, coord_file, type_map_file, force_file, energy_file, mol_identifier):
+def write_to_file(filename, molecule_name, coord_file, type_map_file, type_file, force_file, energy_file, mol_identifier):
     """
     Writes the necessary data to the file according to the provided algorithm.
     """
     with open(filename, 'a') as file:
-        # Write the header
-        file.write("begin\n")
-        file.write(f"comment {molecule_name} ({mol_identifier})\n")
-        file.write("atom ")
 
         # Read the data from input files
-        coords = read_coords(coord_file)
+        geometries = read_geometries(coord_file)
+        coords = geometries[0]
+        num_geom = len(geometries)
         num_atoms = len(coords)
-        elements = read_elements(type_map_file, num_atoms)
+        elements = read_elements(type_map_file, type_file, num_atoms)
         forces = read_forces(force_file)
         energies = read_energy(energy_file)
 
-        # Write the data to the file
-        for i in range(num_atoms):
-            x1, y1, z1 = coords[i]
-            e1 = elements[i]
-            fx1, fy1, fz1 = forces[i]
-            c1, n1 = 0.0, 0.0  # These values are not used
+        for j in range(num_geom):
+            # Write the header
+            file.write("begin\n")
+            file.write(f"comment {molecule_name} ({mol_identifier})\n")
+            file.write("atom ")
+            coords = geometries[j]
+            fcoords = forces[j]
+            # Write the data to the file
+            for i in range(num_atoms):
+                x1, y1, z1 = coords[i]
+                e1 = elements[i]
+                fx1, fy1, fz1 = fcoords[i]
+                c1, n1 = 0.0, 0.0  # These values are not used
 
-            # Write the atom line
-            file.write(f"{x1} {y1} {z1} {e1} {c1} {n1} {fx1} {fy1} {fz1}\n")
+                # Write the atom line
+                file.write(f"{x1} {y1} {z1} {e1} {c1} {n1} {fx1} {fy1} {fz1}\n")
 
-        # Write the energy value from the beginning of the list
-        file.write(f"energy {energies[0]}\n")  # Use the first energy value
+            # Write the energy value from the beginning of the list
+            file.write(f"energy {energies[j]}\n")  # Use the first energy value
 
-        # Write footer
-        file.write("charge 0.0\n")
-        file.write("end\n")
+            # Write footer
+            file.write("charge 0.0\n")
+            file.write("end\n")
         print("wrote to file")
 
-def read_coords(coord_file):
+def read_geometries(coord_file):
     """
-    Reads the coordinates from the coord.raw file and returns them as a list of tuples.
-    Each tuple corresponds to the (x, y, z) coordinates of an atom.
+    Reads the geometries from coord.raw and the coordinates of each geometry.
+    The coordinates of each geometry are converted into a list of tuples,
+    and the geometries are returned as a list of coordinates.
+    In the coordinates each tuple corresponds to the (x, y, z) coordinates of an atom.
     """
-    coords = []
+    geometries = []
     with open(coord_file, 'r') as file:
         lines = file.readlines()
         for line in lines:
+            coords = []
             values = list(map(float, line.split()))
             for i in range(0, len(values), 3):
                 x, y, z = values[i], values[i+1], values[i+2]
                 coords.append((x, y, z))
+            geometries.append(coords)
     print("coords taken")
-    return coords
+    return geometries
 
-def read_elements(type_map_file, num_atoms):
+def read_elements(type_map_file, type_file, num_atoms):
     """
-    Reads the element symbols from the type_map.raw file and repeats them to match the number of atoms.
-    The function returns a list of elements, where each element corresponds to an atom.
+    Reads the element symbols from the type_map.raw file. The element indeces come
+    from type.raw. Then look up the elements names and compile a list of chemical
+    symbols. The function returns a list of elements, where each element corresponds to an atom.
     """
     elements = []
     with open(type_map_file, 'r') as file:
         # Read all element symbols from the file (assuming they are space-separated on a single line)
         element_symbols = file.read().split()
+    with open(type_file, 'r') as file:
+        # Read all element indices from the file
+        element_indeces = file.read().split()
+    if len(element_indeces) != num_atoms:
+        raise RuntimeError(f"Inconsistent number of atoms!\nnum_atoms={str(num_atoms)}\nelement_indeces={str(element_indeces)}")
     
     # Repeat or slice the element symbols to match the number of atoms
     for i in range(num_atoms):
-        elements.append(element_symbols[i % len(element_symbols)])
+        elements.append(element_symbols[int(element_indeces[i])])
     print("element read")
     #print(elements)
     return elements
@@ -88,11 +103,13 @@ def read_forces(force_file):
     with open(force_file, 'r') as file:
         lines = file.readlines()
         for line in lines:
+            fcoords = []
             values = list(map(float, line.split()))
             for i in range(0, len(values), 3):
                 fx, fy, fz = values[i], values[i+1], values[i+2]
-                forces.append((fx, fy, fz))
-    print("forces acquuired")
+                fcoords.append((fx, fy, fz))
+            forces.append(fcoords)
+    print("forces acquired")
     return forces
 
 def read_energy(energy_file):
@@ -105,7 +122,7 @@ def read_energy(energy_file):
         for line in lines:
             energy = float(line.strip())
             energies.append(energy)
-    print("energyyyyy")
+    print("energy")
     return energies
 
 def find_molecule_folders(directory='.'):
@@ -134,17 +151,18 @@ def generate_n2p2_test_files_for_all_folders():
         output_filename = os.path.join(folder_path, f"{molecule_name}_input.data")
         coord_file = os.path.join(folder_path, "coord.raw")
         type_map_file = os.path.join(folder_path, "type_map.raw")
+        type_file = os.path.join(folder_path, "type.raw")
         force_file = os.path.join(folder_path, "force.raw")
         energy_file = os.path.join(folder_path, "energy.raw")
 
-        generate_n2p2_test_file(output_filename, molecule_name, coord_file, type_map_file, force_file, energy_file, mol_identifier)
+        generate_n2p2_test_file(output_filename, molecule_name, coord_file, type_map_file, type_file, force_file, energy_file, mol_identifier)
 
-def generate_n2p2_test_file(output_filename, molecule_name, coord_file, type_map_file, force_file, energy_file, mol_identifier):
+def generate_n2p2_test_file(output_filename, molecule_name, coord_file, type_map_file, type_file, force_file, energy_file, mol_identifier):
     """
     Generates the n2p2 test file by calling the necessary functions.
     """
     create_file(output_filename)
-    write_to_file(output_filename, molecule_name, coord_file, type_map_file, force_file, energy_file, mol_identifier)
+    write_to_file(output_filename, molecule_name, coord_file, type_map_file, type_file, force_file, energy_file, mol_identifier)
 
 # Run the script for all folders
 sample = generate_n2p2_test_files_for_all_folders()
